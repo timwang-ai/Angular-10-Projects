260510d9e26b2f61021cc3e6cd54165e5b2c3866
Aristeidis Bampakos <bampakoa@gmail.com>
2021-01-06 22:38:18 +0200

    Add ngx-wig library

--- CH5/src/app/app.module.ts
+++ CH5/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxWigModule } from 'ngx-wig';
 
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxWigModule
   ],
   providers: [],
   bootstrap: [AppComponent]
