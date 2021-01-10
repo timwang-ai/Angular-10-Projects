896f758ba8a1cd0d5456d6902f28fd86cd65dd4c
Aristeidis Bampakos <bampakoa@gmail.com>
2021-01-10 16:02:24 +0200

    Update webpack configuration file

--- CH5/webpack.config.js
+++ CH5/webpack.config.js
@@ -1,10 +1,9 @@
 const path = require('path');
-const src = path.join(process.cwd(), 'src', 'electron');
 
 module.exports = {
   mode: 'development',
   devtool: 'source-map',
-  entry: path.join(src, 'main.ts'),
+  entry: path.join(process.cwd(), 'src', 'electron', 'main.ts'),
   output: {
     path: path.join(process.cwd(), 'dist', 'my-editor'),
     filename: 'shell.js'
@@ -12,10 +11,7 @@ module.exports = {
   module: {
     rules: [
       {
-        loader: 'ts-loader',
-        options: {
-          configFile: path.join(src, 'tsconfig.json')
-        }
+        loader: 'ts-loader'
       }
     ]
   },
